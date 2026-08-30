36958d1ea670d4af3e1b4b146149443206b3f009
shaheed shaik <shaheed3515@gmail.com>
2026-08-30 17:05:08 +0530

    added using set syntax with commented

--- 05-graphs/06-shortest-path/dijkstra.cpp
+++ 05-graphs/06-shortest-path/dijkstra.cpp
@@ -3,19 +3,37 @@ class Solution {
         void dijkstra(vector<vector<pair<int,int>>>& adj,vector<int>& res,int& src){
             priority_queue<pair<int,int> , vector<pair<int,int>>,greater<pair<int,int>>> pq;
             pq.push({0,src});
+            /*
+            set<pair<int,int>> s;
+            s.insert({0,src});
+            */
             res[src]=0;
             while(!pq.empty()){
                 pair<int,int> t=pq.top();
                 int d=t.first;
                 int v=t.second;
                 pq.pop();
+                /*
+                pair<int,int> it=*s.begin(); // begin() is a iterator so dereference it.
+                int d=it.first;
+                int v=it.second;
+                s.erase(it);
+                */
                 if(d>res[v]) continue; //if the initial moving dist is geater than in res it waste to take that path
                 for( pair<int,int>& x : adj[v]){
                     int dis=x.first;
                     int edge=x.second;
                     if(dis+d<res[edge]){
+                        /*
+                        if(res[edge]!=INT_MAX){
+                            s.erase({res[edge],edge});
+                        }
+                        */
                         res[edge]=dis+d;
                         pq.push({dis+d,edge});
+                        /*
+                        s.insert({dis+d,edge});
+                        */
                     }
                 }
             }
